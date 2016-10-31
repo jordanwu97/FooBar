df2de0ed47c7606e2dd60ccd4f5b843f24a3ddcf
jordanwu97 <Jordan@Jordans-MacBook-Pro.local>
2016-10-30 20:23:25 -0500

    added unoptimized version of stairs counter

--- stairs_optimized.py
+++ stairs_optimized.py
@@ -3,6 +3,8 @@ import math as m
 # http://www.learningace.com/doc/2568736/b42b417e56ba21d3df050ae1ec4496ba/partitionsq
 # Used the algorithm described in this
 
+csum = 0
+
 sample = [1, 1, 1, 2, 2, 3, 4, 5]
 
 def q(n,pos,level):
@@ -39,7 +41,7 @@ def sigma(n):
 			return (-1)**j
 	return 0
 
-print(answer(9))
+print(answer(30))
 
 # verify = [0, 0, 0, 1, 1, 2, 3, 4, 5, 7, 9, 11, 14, 17, 21, 26, 31, 37, 45, 53, 63, 75, 88, 103, 121, 141, 164, 191, 221, 255, 295, 339, 389, 447, 511, 584, 667, 759, 863, 981, 1112, 1259, 1425, 1609, 1815, 2047, 2303, 2589, 2909, 3263, 3657, 4096, 4581, 5119, 5717, 6377]
 
---
+++ stairs_unoptimized.py
@@ -0,0 +1,31 @@
+import math as m
+
+sample = [	1, 1, 1, 2, 2, 3, 4, 5, 6, 8, 10, 12, 15, 18, 22, 27, 32, 38, 46, 54]
+
+def sigma(n):
+	for j in range(1,n):
+		if j*(3*j+1)/2 == n or j*(3*j-1)/2 == n:
+			return (-1)**j
+	return 0
+
+def q(n):
+
+	if(n<len(sample)):
+
+		# print(n,sample[n])
+		return sample[n]
+
+	top = int(m.sqrt(n))
+
+	s = 0
+
+	for k in range(1,top+1):
+		# print(str(n)+' - '+str(k**2))
+
+		# print(2*((-1)**(k+1))*q(n-k**2))
+
+		s = s + (2*((-1)**(k+1))*q(n-k**2))
+
+	return s + sigma(n)
+
+print(q(30))
