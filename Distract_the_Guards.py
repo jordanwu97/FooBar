1c191c86429aac18a0ddd6c6315f3d242f9bd6ea
Jordan <jordanw2@illinois.edu>
2016-11-02 16:30:27 -0500

    fixed edge formation algorithm, but will need to redo since it is wayyy to slow

--- Distract_the_Guards.py
+++ Distract_the_Guards.py
@@ -1,41 +1,47 @@
 from random import randint
 
-def test(a,b,lev,prevk):
-	
-	# print(a,b,lev,prevk)
+l = set()
 
-	if a == b:
-		print('exit')
-		return True
+def test(a,b):
+	k = (a,b)
 
-	if a == prevk:
-		print('did not exit')
-		return False
-	
-	elif a > b:
-		prev = a
-		a -= 1
-		b += 1
-		test(a,b,lev+1,prev)
-	
-	elif b > a:
-		prev = b
-		a += 1
-		b -= 1
-		test(a,b,lev+1,prev)
+	while True:
+
+		# print(a,b)
+
+		if (b,a) in l or (a,b) in l:
+			# print('did not exit')
+			return (False,k)
+
+		if a == b:
+			# print('exit')
+			return (True,k)
+
+		if a > b:
+			l.add((a,b))
+			prev = a
+			a -= b
+			b += b
+		elif b > a:
+			l.add((a,b))
+			prev = b
+			b -= a
+			a += a
 
 def createrandpair():
-	a = randint(0,23453)
-	b = randint(0,23123)
-	return (a,a+2*b+1)
+	a = randint(0,4534)
+	b = randint(0,4583)
+	return (a,b)
 
-k = createrandpair()
+def verify():
+	n = False
+	for _ in range(0,100):
+		k = createrandpair()
+		# print(k)
+		a = test(k[0],k[1])
+		if(a[0]):
+			n = True;
+			print(a[1])
+	print(n)
 
-test(k[0],k[1],0,0)
-
-for _ in range(0,1000):
-	k = createrandpair()
-	print(k)
-	if(test(k[0],k[1],0,0)):
-		print(True)
-print(False)
+verify()
