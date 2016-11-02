f947f8a63707c3512fb09d859789fc06815b7323
Jordan <jordanw2@illinois.edu>
2016-11-02 17:55:19 -0500

    Trying to find pattern GG

--- Distract_the_Guards.py
+++ Distract_the_Guards.py
@@ -7,7 +7,7 @@ def test(a,b):
 
 	while True:
 
-		# print(a,b)
+		print(a,b)
 
 		if (b,a) in l or (a,b) in l:
 			# print('did not exit')
@@ -28,20 +28,33 @@ def test(a,b):
 			b -= a
 			a += a
 
+def test2(a,b):
+	k = (a,b)
+
+
 def createrandpair():
 	a = randint(0,4534)
 	b = randint(0,4583)
 	return (a,b)
 
 def verify():
+	t = open('trues', 'w')
+	f = open('falses', 'w')
 	n = False
-	for _ in range(0,100):
+	for _ in range(0,1000):
 		k = createrandpair()
 		# print(k)
 		a = test(k[0],k[1])
 		if(a[0]):
 			n = True;
 			print(a[1])
+			t.write(str(a[1])+"\n")
+		else:
+			f.write(str(a[1])+"\n")
 	print(n)
+	t.close()
+	f.close()
 
-verify()
+# verify()
+
+print(test(1343,2014))
